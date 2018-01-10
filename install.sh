d015d4b4995569cd98e3e88fcd6d99cc8417d6b3
KatherineAdair <omshisamal72@gmail.com>
2018-01-10 16:53:22 +0530

    Added lsb-core package
    Absence was causing configuration problems in Google Earth install

--- install.sh
+++ install.sh
@@ -117,7 +117,7 @@ echo "Upgrading system packages ..."
 sudo apt-get -yq upgrade
 
 echo "Installing new packages..."
-sudo apt-get install -yq git chromium-browser nautilus openssh-server sshpass squid3 squid-cgi apache2 xdotool unclutter
+sudo apt-get install -yq git chromium-browser nautilus openssh-server sshpass squid3 squid-cgi apache2 xdotool unclutter lsb-core
 
 echo "Installing Google Earth..."
 wget -q $EARTH_DEB
